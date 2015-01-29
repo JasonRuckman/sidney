cbdb31c39aa7e3d3f38aa5b6fc74a8e85596d29a
JasonRuckman <jason.ruckman@gmail.com>
2015-01-28 20:46:30 -0800

    Don't need type bindings with typetags.

--- scala/src/main/scala/org/sidney/scala/ScalaTypeRefBuilder.scala
+++ scala/src/main/scala/org/sidney/scala/ScalaTypeRefBuilder.scala
@@ -19,10 +19,10 @@ object ScalaTypeRefBuilder {
   val arrClass = Class.forName("scala.Array")
 
   def typeRef[T]()(implicit tag: TypeTag[T]) = {
-    decompose(tag.tpe, null, false)
+    decompose(tag.tpe, false)
   }
 
-  def decompose(t: Type, parentBindings: TypeBindings, isTypeArg: Boolean): core.TypeRef = {
+  def decompose(t: Type, isTypeArg: Boolean): core.TypeRef = {
     val typeArgs = t match {
       case TypeRef(_, _, args) => args
     }
@@ -30,7 +30,7 @@ object ScalaTypeRefBuilder {
       case TypeRef(_, arg, _) => arg
     }
 
-    decompose(t, rawType, typeArgs, parentBindings, isTypeArg, t.members.flatMap(x => {
+    decompose(t, rawType, typeArgs, isTypeArg, t.members.flatMap(x => {
       if(x.isTerm) {
         val n = x.asTerm
         if((n.isVal || n.isVar) && !n.isStatic) {
@@ -44,19 +44,14 @@ object ScalaTypeRefBuilder {
     }).toSeq)
   }
 
-  def decompose(t : Type, sym : Symbol, typeArgs: List[Type], parentBindings: TypeBindings, isTypeArg: Boolean, members : Seq[TermSymbol]): core.TypeRef = {
+  def decompose(t : Type, sym : Symbol, typeArgs: List[Type], isTypeArg: Boolean, members : Seq[TermSymbol]): core.TypeRef = {
     val ref = new core.TypeRef(adapt(sym.fullName, isTypeArg))
     //now we have the class for the symbol, go decompose the typeargs, then go through all the fields and bind them to the type args
     var bindings: TypeBindings = null
     val typeArgsAsRefs = typeArgs.map(x => {
-      decompose(x, parentBindings, !isPrimitiveArray(x, ref))
+      decompose(x, !isPrimitiveArray(x, ref))
     })
     typeArgsAsRefs.foreach(ref.addTypeParameter)
-
-    bindings = typeArgs.length match {
-      case 0 => Types.binding(ref.getType, parentBindings)
-      case _ => Types.binding(ref)
-    }
 
     val availableFields = ref.getType.getDeclaredFields.filterNot(f => {
       Modifier.isStatic(f.getModifiers) || Modifier.isTransient(f.getModifiers)
@@ -65,7 +60,7 @@ object ScalaTypeRefBuilder {
     members.foreach(member => {
       val name = member.name.decoded.trim
       val memberType = member.typeSignatureIn(t)
-      val memberRef = decompose(memberType, bindings, false)
+      val memberRef = decompose(memberType, false)
       availableFields.get(name).foreach(field => {
         ref.addField(
           memberRef.toField(field)
@@ -83,7 +78,7 @@ object ScalaTypeRefBuilder {
   }
 
   private def isPrimitiveArray(t : Type, ref : org.sidney.core.TypeRef) : Boolean = {
-    val rawType = decompose(t, null, false)
+    val rawType = decompose(t, false)
     ScalaPrimitiveTypeAdapter.isJavaPrimitive(rawType.getType) && ref.getType.getName.eq("scala.Array")
   }
 }
